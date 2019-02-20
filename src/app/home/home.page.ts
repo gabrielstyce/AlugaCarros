268d1f3f6fb97b2e655ea7e05e9bb32f09c2c0cc
Gabriel Araujo <42006404+GabrielStyce@users.noreply.github.com>
2019-02-19 22:16:51 -0300

    implementacoes

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit{
+  public carros;
 
+  constructor(){
+  }
+  
+  ngOnInit(): void {
+    this.carros = [
+      {
+        "nome": "Fusca",
+        "preco": 1000
+      },{
+        "nome": "Ferrari",
+        "preco": 2000
+      },{
+        "nome": "Gol",
+        "preco": 2000
+      },{
+        "nome": "Astra",
+        "preco": 3000
+      }]
+  }
 }
